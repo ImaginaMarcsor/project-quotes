f0ddc05619377cc96e641be3f52408cfca4e9241
ImaginaMarcsor <marcosr@imaginacolombia.com>
2020-08-13 17:31:24 -0500

    update 130820

--- src/components/custom/header.vue
+++ src/components/custom/header.vue
@@ -1,10 +1,33 @@
 <template>
-  <div class="q-container">
-    <div class="desktop-only">
+  <div>
+    <div class="desktop-only q-container">
       <img :src="logo" class="q-mt-md" >
+      <q-no-ssr>
+        <div class="absolute-top-right" style="top: 10px; right: 10px;">
+          <q-btn-group rounded>
+            <q-btn :to="{name: 'app.admin.home'}" push no-caps v-if="quserState.authenticated" rounded>
+              <q-avatar size="25px">
+                <img :src="quserState.userData.mainImage">
+              </q-avatar>
+              <div class="q-ml-xs">
+                {{quserState.userData.firstName}}
+              </div>
+            </q-btn>
+            <q-btn :to="{name: 'auth.logout'}" push no-caps v-if="quserState.authenticated" rounded>
+              <q-icon color="negative" name="fas fa-sign-out-alt" size="20px"/>
+            </q-btn>
+            <q-btn v-else dense flat no-caps :to="{name: 'auth.login'}" class="no-shadow">
+              <q-icon color="negative" name="fas fa-sign-in-alt" size="20px"/>
+            </q-btn>
+          </q-btn-group>
+
+        </div>
+      </q-no-ssr>
     </div>
-    <q-no-ssr>
-      <div class="absolute-top-right" style="top: 10px; right: 10px;">
+    <div class="mobile-only">
+      <q-toolbar class="text-black bg-white shadow-1">
+        <img :src="logo" style="width: 20%; max-width: 100px; margin: 10px auto">
+        <q-no-ssr>
         <q-btn-group rounded>
           <q-btn :to="{name: 'app.admin.home'}" push no-caps v-if="quserState.authenticated" rounded>
             <q-avatar size="25px">
@@ -17,17 +40,11 @@
           <q-btn :to="{name: 'auth.logout'}" push no-caps v-if="quserState.authenticated" rounded>
             <q-icon color="negative" name="fas fa-sign-out-alt" size="20px"/>
           </q-btn>
-	        <q-btn v-else dense flat no-caps :to="{name: 'auth.login'}" class="no-shadow">
-		        <q-icon color="negative" name="fas fa-sign-in-alt" size="20px"/>
-	        </q-btn>
+          <q-btn v-else dense flat no-caps :to="{name: 'auth.login'}" class="no-shadow">
+            <q-icon color="negative" name="fas fa-sign-in-alt" size="20px"/>
+          </q-btn>
         </q-btn-group>
-	      
-      </div>
-    </q-no-ssr>
-    <div class="mobile-only">
-      <q-toolbar class="text-black bg-white shadow-1">
-        <img :src="logo" style="width: 20%">
-        <q-toolbar-title>{{projectName}}</q-toolbar-title>
+        </q-no-ssr>
       </q-toolbar>
     </div>
   </div>
